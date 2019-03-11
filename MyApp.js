e2a2fd9a043d47b2b6c3113b84a42edeb3b27a3f
Rahulggsipu <chetangoyal7503@gmail.com>
2019-03-11 22:12:34 +0530

    Move Component into seprate file (MyApp.js)

---
+++ MyApp.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MyApp(){
+    return (
+    <div>
+        <h1>This is my app</h1>
+       <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+       </ul>
+    </div>
+ )
+}
+
+export default MyApp
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function MyApp(){
-    return (
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
- )
-}
+import MyApp from "./Myapp"
 ReactDOM.render(<MyApp />,document.getElementById("root"))
